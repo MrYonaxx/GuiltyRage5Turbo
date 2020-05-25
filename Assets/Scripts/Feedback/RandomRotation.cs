3ba802065c76c3f3ab8aa0b1a0e55d35d4580de3
Nam <nam.mirbeau@gmail.com>
2020-05-26 01:13:09 +0200

    particle random

---
+++ Assets/Scripts/Feedback/RandomRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        this.transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+    }
+}
